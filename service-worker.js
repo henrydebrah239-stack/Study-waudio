c44927a49a8b287dc5fb87e97ce139ababe9bce2
henrydebrah239-stack <henrydebrah239@gmail.com>
2025-10-27 01:51:42 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'study-audio-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        // Cache the files needed for the app shell to function
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Try to serve assets from cache first.
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+￼Enter
